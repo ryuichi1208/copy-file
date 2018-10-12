91112930fa75e41f99168b515b8f0e1ed9f95e78
ryuichi1208 <ryucrosskey@gmail.com>
2018-10-12 23:10:12 +0900

    dir add

--- src/include/common.h
+++ src/include/common.h
@@ -5,10 +5,18 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <getopt.h>
 
 /* 定数 */
 #define MAX_FILE_SIZE   256
 #define MIN_FILE SIZE   1
+#define SERVICE_PORT	8080
+
+/* Alias */
+typedef unsigned long	u_long;
+typedef unsigned int 	u_int;
+typedef unsigned short 	u_short;
 
 /* グローバル変数 */
 
